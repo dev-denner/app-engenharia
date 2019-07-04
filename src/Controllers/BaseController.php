8a365b49c57bba073e1307b8aa1cc610171fdb31
Denner Fernandes <denners777@hotmail.com>
2019-07-04 01:46:18 -0300

    acertando redirect do login
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -16,7 +16,9 @@ class BaseController
         $this->data['url'] = getenv('BASEURI');
         $this->flash       = $container->get('flash');
 
-        if ($_SERVER['REQUEST_URI'] !== '/login') {
+        $uri = explode('/', $_SERVER['REQUEST_URI']);
+
+        if (end($uri) !== 'login') {
             if (!isset($_SESSION['user_logged_in'])) {
                 header('location: ' . $this->data['url'] . 'login');
                 exit();
